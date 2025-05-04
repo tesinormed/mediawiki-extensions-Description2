c9dd2aa91a37ab33f0c239b27a03a5eda3aa2e7c
tesinormed <tesinormed@5505.industries>
2025-05-03 17:34:30 -0700

    version 0.6.0

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -2,34 +2,34 @@
 
 namespace MediaWiki\Extension\Description2;
 
-use Config;
-use ConfigFactory;
+use MediaWiki\Api\Hook\ApiOpenSearchSuggestHook;
+use MediaWiki\Config\Config;
+use MediaWiki\Config\ConfigFactory;
 use MediaWiki\Hook\OutputPageParserOutputHook;
 use MediaWiki\Hook\ParserAfterTidyHook;
 use MediaWiki\Hook\ParserFirstCallInitHook;
-use Parser;
+use MediaWiki\Page\PageProps;
+use MediaWiki\Parser\Parser;
+use MediaWiki\Rest\Hook\SearchResultProvideDescriptionHook;
 
 class Hooks implements
 	ParserAfterTidyHook,
 	ParserFirstCallInitHook,
-	OutputPageParserOutputHook
+	OutputPageParserOutputHook,
+	ApiOpenSearchSuggestHook,
+	SearchResultProvideDescriptionHook
 {
-	/** @var Config */
 	private Config $config;
-
-	/** @var DescriptionProvider */
+	private PageProps $pageProps;
 	private DescriptionProvider $descriptionProvider;
-
-	/** @var int */
 	private int $maximumLength;
 
-	/**
-	 * @param ConfigFactory $configFactory
-	 */
 	public function __construct(
-		ConfigFactory $configFactory
+		ConfigFactory $configFactory,
+		PageProps $pageProps,
 	) {
 		$this->config = $configFactory->makeConfig( 'description2' );
+		$this->pageProps = $pageProps;
 		$this->descriptionProvider = new DescriptionProvider( $this->config->get( 'Description2IgnoreSelectors' ) );
 		$this->maximumLength = $this->config->get( 'Description2MaximumLength' );
 	}
@@ -89,5 +89,35 @@ class Hooks implements
 		if ( $description !== null ) {
 			$outputPage->addMeta( 'description', $description );
 		}
+	}
+
+	/**
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/ApiOpenSearchSuggest
+	 * @inheritDoc
+	 */
+	public function onApiOpenSearchSuggest( &$results ): void {
+		$titles = array_map( static fn ( $result ) => $result['title'], $results );
+		foreach ( self::getDescriptions( $titles ) as $id => $description ) {
+			if ( $description === null ) {
+				continue;
+			}
+
+			$results[$id]['extract'] = $description;
+			$results[$id]['extract trimmed'] = false;
+		}
+	}
+
+	/**
+	 * @link https://www.mediawiki.org/wiki/Manual:Hooks/SearchResultProvideDescription
+	 * @inheritDoc
+	 */
+	public function onSearchResultProvideDescription( array $pageIdentities, &$descriptions ): void {
+		foreach ( self::getDescriptions( $pageIdentities ) as $id => $description ) {
+			$descriptions[$id] = $description;
+		}
+	}
+
+	private function getDescriptions( array $titles ): array {
+		return $this->pageProps->getProperties( $titles, 'description' );
 	}
 }
